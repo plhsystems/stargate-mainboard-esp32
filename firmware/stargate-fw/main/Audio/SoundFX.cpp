ffd19c8203b7e87216ef6bdce7a100dc10a70a9d
Max <pinkymaxou08@gmail.com>
2024-01-14 15:32:06 -0500

    Template for sound FX

---
+++ firmware/stargate-fw/main/Audio/SoundFX.cpp
@@ -0,0 +1,25 @@
+#include "SoundFX.hpp"
+
+SoundFX::SoundFX()
+{
+}
+
+void SoundFX::Init()
+{
+
+}
+
+void SoundFX::Start()
+{
+    // TODO: Create a task for i2s
+}
+
+void SoundFX::PlaySound(File soundFile, bool bRepeat)
+{
+    // TODO: Fill this
+}
+
+void SoundFX::StopSound()
+{
+    // TODO: Fill this
+}
