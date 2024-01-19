a84f3273ee05ff2618252eaa2020ae0c89a4932c
Max <pinkymaxou08@gmail.com>
2024-01-19 17:07:26 -0500

    Address for the pegasus galaxy

--- firmware/stargate-fw/main/Gate/PegasusGate.hpp
+++ firmware/stargate-fw/main/Gate/PegasusGate.hpp
@@ -27,9 +27,25 @@ class PegasusGate : public BaseGate
     }
     int32_t GetAddressCount() override { return sizeof(m_gateAddresses)/sizeof(m_gateAddresses[0]); }
 
+    // TODO: Add https://stargate.fandom.com to the thanks list
     static inline const GateAddress m_gateAddresses[] =
     {
-
+        { "Asuras", 1, 6, 25, 34, 16, 30 },   // https://stargate.fandom.com/wiki/Asuras
+        { "Lantea", 11, 17, 13, 22, 9, 1 },   // https://stargate.fandom.com/wiki/Lantea
+        { "Wraith homeworld", 26, 12, 1, 24, 12, 8 },   // https://stargate.fandom.com/wiki/Wraith_homeworld
+        { "Genii homeworld", 36, 15, 13, 24, 3, 23 }, // https://stargate.fandom.com/wiki/Genii_homeworld
+        { "Taranian settlement", 1, 15, 29, 2, 13, 23 },  // https://stargate.fandom.com/wiki/Taranian_settlement
+        { "Lucius' planet", 1, 23, 30, 32, 20, 18 },   // https://stargate.fandom.com/wiki/Lucius%27_planet
+        { "M1B-129", 23, 32, 30, 1, 18, 20 },  // https://stargate.fandom.com/wiki/M1B-129
+        { "M65-PL8", 28, 27, 31, 16, 33, 17 }, // https://stargate.fandom.com/wiki/M65-PL8
+        { "M4X-337", 2, 5, 15, 8, 31, 33 }, // https://stargate.fandom.com/wiki/M4X-337
+        { "Olesia", 1, 8, 2, 32, 9, 21 }, // https://stargate.fandom.com/wiki/Olesia
+        { "Vedeena", 27, 35, 29, 22, 10, 13 }, // https://stargate.fandom.com/wiki/Vedeena
+        // { "Thenora" }, // INCOMPLETE https://stargate.fandom.com/wiki/Thenora
+        // { "Talus" }, // INCOMPLETE https://stargate.fandom.com/wiki/Talus_(planet)
+        // { "Proculus" },// INCOMPLETE https://stargate.fandom.com/wiki/Proculus
+        // { "Lord Protector's Planet" }, // INCOMPLETE https://stargate.fandom.com/wiki/Lord_Protector%27s_Planet
+        // { "Ford's Planet" }, // INCOMPLETE https://stargate.fandom.com/wiki/Ford%27s_planet
     };
 
     static inline const GateSymbol m_symbols[36]
