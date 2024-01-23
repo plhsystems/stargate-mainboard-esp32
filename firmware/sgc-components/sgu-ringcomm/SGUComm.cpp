66cd6ff7bad44991d4181255b3aa028781de7f13
Max <pinkymaxou08@gmail.com>
2024-01-22 20:23:20 -0500

    SGU comm in C++

--- firmware/sgc-components/sgu-ringcomm/SGUComm.cpp
+++ firmware/sgc-components/sgu-ringcomm/SGUComm.cpp
@@ -6,14 +6,10 @@
 #define MAGIC1 0xEF
 #define MAGIC_LENGTH 2
 
-void SGUBRPROTOCOL_Init(SGUBRPROTOCOL_SHandle* psHandle, const SGUBRPROTOCOL_SConfig* psConfig)
-{
-    psHandle->psConfig = psConfig;
-}
+using namespace SGUCommNS;
 
-bool SGUBRPROTOCOL_Decode(const SGUBRPROTOCOL_SHandle* psHandle, const uint8_t* u8Datas, uint16_t u16Length)
+bool SGUComm::Decode(const SConfig& refConfig, const uint8_t* u8Datas, uint16_t u16Length)
 {
-    assert(psHandle->psConfig != NULL);
     if (u16Length < MAGIC_LENGTH + 1)
         return false;
 
@@ -22,22 +18,22 @@ bool SGUBRPROTOCOL_Decode(const SGUBRPROTOCOL_SHandle* psHandle, const uint8_t* 
 
     u8Datas += MAGIC_LENGTH; // Skip header
     u16Length -= MAGIC_LENGTH;
-    const SGUBRPROTOCOL_ECMD eCmd = (SGUBRPROTOCOL_ECMD)u8Datas[0];
+    const EProtocolCmd eCmd = (EProtocolCmd)u8Datas[0];
     switch(eCmd)
     {
-        case SGUBRPROTOCOL_ECMD_KeepAlive:
+        case EProtocolCmd::KeepAlive:
         {
             if (u16Length < 5)
                 return false;
-            const SGUBRPROTOCOL_SKeepAliveArg sKeepAlive =
+            const SKeepAliveArg sKeepAlive =
             {
                 .u32MaximumTimeMS = (uint32_t)((u8Datas[1] << 24U) | (u8Datas[2] << 16U) | (u8Datas[3] << 8U) | u8Datas[4])
             };
-            if (psHandle->psConfig->fnKeepAliveHandler != NULL)
-                psHandle->psConfig->fnKeepAliveHandler(&sKeepAlive);
+            if (refConfig.fnKeepAliveHandler != NULL)
+                refConfig.fnKeepAliveHandler(&sKeepAlive);
             return true;
         }
-        case SGUBRPROTOCOL_ECMD_UpdateLight:
+        case EProtocolCmd::UpdateLight:
         {
             // CMD | R | G | B | LIGHT0 | LIGHT1 | ...
             if (u16Length < 5)
@@ -46,60 +42,60 @@ bool SGUBRPROTOCOL_Decode(const SGUBRPROTOCOL_SHandle* psHandle, const uint8_t* 
             // Lights
             uint8_t u8LightCount = u16Length - 4;
 
-            const SGUBRPROTOCOL_SUpdateLightArg sUpdateLightArg =
+            const SUpdateLightArg sUpdateLightArg =
             {
-                .sRGB = { .u8Red = u8Datas[1], .u8Green = u8Datas[2], .u8Blue = u8Datas[3] },
+                .sColor = { .u8Red = u8Datas[1], .u8Green = u8Datas[2], .u8Blue = u8Datas[3] },
                 .u8Lights = u8Datas+4,
                 .u8LightCount = u8LightCount
             };
 
-            if (psHandle->psConfig->fnUpdateLightHandler != NULL)
-                psHandle->psConfig->fnUpdateLightHandler(&sUpdateLightArg);
+            if (refConfig.fnUpdateLightHandler != NULL)
+                refConfig.fnUpdateLightHandler(&sUpdateLightArg);
 
             return true;
         }
-        case SGUBRPROTOCOL_ECMD_TurnOff:
+        case EProtocolCmd::TurnOff:
         {
-            if (psHandle->psConfig->fnTurnOffHandler != NULL)
-                psHandle->psConfig->fnTurnOffHandler();
+            if (refConfig.fnTurnOffHandler != NULL)
+                refConfig.fnTurnOffHandler();
             return true;
         }
-        case SGUBRPROTOCOL_ECMD_GotoFactory:
+        case EProtocolCmd::GotoFactory:
         {
-            if (psHandle->psConfig->fnGotoFactoryHandler != NULL)
-                psHandle->psConfig->fnGotoFactoryHandler();
+            if (refConfig.fnGotoFactoryHandler != NULL)
+                refConfig.fnGotoFactoryHandler();
             return true;
         }
-        case SGUBRPROTOCOL_ECMD_GotoOTAMode:
+        case EProtocolCmd::GotoOTAMode:
         {
-            if (psHandle->psConfig->fnGotoOTAModeHandler != NULL)
-                psHandle->psConfig->fnGotoOTAModeHandler();
+            if (refConfig.fnGotoOTAModeHandler != NULL)
+                refConfig.fnGotoOTAModeHandler();
             return true;
         }
-        case SGUBRPROTOCOL_ECMD_ChevronsLightning:
+        case EProtocolCmd::ChevronsLightning:
         {
             // CMD | ANIM ...
             if (u16Length < 2)
                 return false;
 
-            const SGUBRPROTOCOL_ECHEVRONANIM eChevronAnim = (SGUBRPROTOCOL_ECHEVRONANIM)u8Datas[1];
-            if (eChevronAnim >= SGUBRPROTOCOL_ECHEVRONANIM_Count)
+            const EChevronAnimation eChevronAnim = (EChevronAnimation)u8Datas[1];
+            if (eChevronAnim >= EChevronAnimation::Count)
                 return false;
 
-            const SGUBRPROTOCOL_SChevronsLightningArg sChevronsLightningArg =
+            const SChevronsLightningArg sChevronsLightningArg =
             {
                 .eChevronAnim = eChevronAnim
             };
 
-            if (psHandle->psConfig->fnChevronsLightningHandler != NULL)
-                psHandle->psConfig->fnChevronsLightningHandler(&sChevronsLightningArg);
+            if (refConfig.fnChevronsLightningHandler != NULL)
+                refConfig.fnChevronsLightningHandler(&sChevronsLightningArg);
             return true;
         }
     }
     return false;
 }
 
-uint32_t SGUBRPROTOCOL_EncKeepAlive(uint8_t* u8Dst, uint16_t u16MaxLen, const SGUBRPROTOCOL_SKeepAliveArg* psArg)
+uint32_t SGUComm::EncKeepAlive(uint8_t* u8Dst, uint16_t u16MaxLen, const SKeepAliveArg* psArg)
 {
     const uint16_t u16ReqLength = MAGIC_LENGTH+5;
     if (u16MaxLen < u16ReqLength)
@@ -107,7 +103,7 @@ uint32_t SGUBRPROTOCOL_EncKeepAlive(uint8_t* u8Dst, uint16_t u16MaxLen, const SG
 
     u8Dst[0] = MAGIC0;
     u8Dst[1] = MAGIC1;
-    u8Dst[2] = (uint8_t)SGUBRPROTOCOL_ECMD_KeepAlive;
+    u8Dst[2] = (uint8_t)EProtocolCmd::KeepAlive;
     u8Dst[3] = (uint8_t)(psArg->u32MaximumTimeMS >> 24U);
     u8Dst[4] = (uint8_t)(psArg->u32MaximumTimeMS >> 16U);
     u8Dst[5] = (uint8_t)(psArg->u32MaximumTimeMS >> 8U);
@@ -115,7 +111,7 @@ uint32_t SGUBRPROTOCOL_EncKeepAlive(uint8_t* u8Dst, uint16_t u16MaxLen, const SG
     return u16ReqLength;
 }
 
-uint32_t SGUBRPROTOCOL_EncTurnOff(uint8_t* u8Dst, uint16_t u16MaxLen)
+uint32_t SGUComm::EncTurnOff(uint8_t* u8Dst, uint16_t u16MaxLen)
 {
     const uint16_t u16ReqLength = MAGIC_LENGTH+1;
     if (u16MaxLen < u16ReqLength)
@@ -123,11 +119,11 @@ uint32_t SGUBRPROTOCOL_EncTurnOff(uint8_t* u8Dst, uint16_t u16MaxLen)
 
     u8Dst[0] = MAGIC0;
     u8Dst[1] = MAGIC1;
-    u8Dst[2] = (uint8_t)SGUBRPROTOCOL_ECMD_TurnOff;
+    u8Dst[2] = (uint8_t)EProtocolCmd::TurnOff;
     return u16ReqLength;
 }
 
-uint32_t SGUBRPROTOCOL_EncGotoFactory(uint8_t* u8Dst, uint16_t u16MaxLen)
+uint32_t SGUComm::EncGotoFactory(uint8_t* u8Dst, uint16_t u16MaxLen)
 {
     const uint16_t u16ReqLength = MAGIC_LENGTH+1;
     if (u16MaxLen < u16ReqLength)
@@ -135,11 +131,11 @@ uint32_t SGUBRPROTOCOL_EncGotoFactory(uint8_t* u8Dst, uint16_t u16MaxLen)
 
     u8Dst[0] = MAGIC0;
     u8Dst[1] = MAGIC1;
-    u8Dst[2] = (uint8_t)SGUBRPROTOCOL_ECMD_GotoFactory;
+    u8Dst[2] = (uint8_t)EProtocolCmd::GotoFactory;
     return u16ReqLength;
 }
 
-uint32_t SGUBRPROTOCOL_EncGotoOTAMode(uint8_t* u8Dst, uint16_t u16MaxLen)
+uint32_t SGUComm::EncGotoOTAMode(uint8_t* u8Dst, uint16_t u16MaxLen)
 {
     const uint16_t u16ReqLength = MAGIC_LENGTH+1;
     if (u16MaxLen < u16ReqLength)
@@ -147,11 +143,11 @@ uint32_t SGUBRPROTOCOL_EncGotoOTAMode(uint8_t* u8Dst, uint16_t u16MaxLen)
 
     u8Dst[0] = MAGIC0;
     u8Dst[1] = MAGIC1;
-    u8Dst[2] = (uint8_t)SGUBRPROTOCOL_ECMD_GotoOTAMode;
+    u8Dst[2] = (uint8_t)EProtocolCmd::GotoOTAMode;
     return u16ReqLength;
 }
 
-uint32_t SGUBRPROTOCOL_EncUpdateLight(uint8_t* u8Dst, uint16_t u16MaxLen, const SGUBRPROTOCOL_SUpdateLightArg* psArg)
+uint32_t SGUComm::EncUpdateLight(uint8_t* u8Dst, uint16_t u16MaxLen, const SUpdateLightArg* psArg)
 {
     const uint16_t u16ReqLength = MAGIC_LENGTH + /*Cmd*/1 + /*R|G|B*/3 + /*Light indexes...*/psArg->u8LightCount;
     if (u16MaxLen < u16ReqLength)
@@ -159,15 +155,15 @@ uint32_t SGUBRPROTOCOL_EncUpdateLight(uint8_t* u8Dst, uint16_t u16MaxLen, const 
 
     u8Dst[0] = MAGIC0;
     u8Dst[1] = MAGIC1;
-    u8Dst[2] = (uint8_t)SGUBRPROTOCOL_ECMD_UpdateLight;
-    u8Dst[3] = psArg->sRGB.u8Red;
-    u8Dst[4] = psArg->sRGB.u8Green;
-    u8Dst[5] = psArg->sRGB.u8Blue;
+    u8Dst[2] = (uint8_t)EProtocolCmd::UpdateLight;
+    u8Dst[3] = psArg->sColor.u8Red;
+    u8Dst[4] = psArg->sColor.u8Green;
+    u8Dst[5] = psArg->sColor.u8Blue;
     memcpy(u8Dst+6, psArg->u8Lights, psArg->u8LightCount);
     return u16ReqLength;
 }
 
-uint32_t SGUBRPROTOCOL_EncChevronLightning(uint8_t* u8Dst, uint16_t u16MaxLen, const SGUBRPROTOCOL_SChevronsLightningArg* psArg)
+uint32_t SGUComm::EncChevronLightning(uint8_t* u8Dst, uint16_t u16MaxLen, const SChevronsLightningArg* psArg)
 {
     const uint16_t u16ReqLength = MAGIC_LENGTH + /*Cmd*/1 + /*Anim Type*/1;
     if (u16MaxLen < u16ReqLength)
@@ -175,7 +171,7 @@ uint32_t SGUBRPROTOCOL_EncChevronLightning(uint8_t* u8Dst, uint16_t u16MaxLen, c
 
     u8Dst[0] = MAGIC0;
     u8Dst[1] = MAGIC1;
-    u8Dst[2] = (uint8_t)SGUBRPROTOCOL_ECMD_ChevronsLightning;
+    u8Dst[2] = (uint8_t)EProtocolCmd::ChevronsLightning;
     u8Dst[3] = (uint8_t)psArg->eChevronAnim;
     return u16ReqLength;
 }
--- firmware/sgc-components/sgu-ringcomm/SGUComm.hpp
+++ firmware/sgc-components/sgu-ringcomm/SGUComm.hpp
@@ -1,100 +1,88 @@
-#ifndef _SGUBRPROTOCOL_H_
-#define _SGUBRPROTOCOL_H_
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
+#pragma once
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef enum
+namespace SGUCommNS
 {
-    SGUBRPROTOCOL_ECMD_KeepAlive = 0,
-    SGUBRPROTOCOL_ECMD_TurnOff = 1,
-    SGUBRPROTOCOL_ECMD_UpdateLight = 2,
-    SGUBRPROTOCOL_ECMD_ChevronsLightning = 3,
-    SGUBRPROTOCOL_ECMD_GotoFactory = 4,
-    SGUBRPROTOCOL_ECMD_GotoOTAMode = 5,
-} SGUBRPROTOCOL_ECMD;
+    enum class EProtocolCmd
+    {
+        KeepAlive = 0,
+        TurnOff = 1,
+        UpdateLight = 2,
+        ChevronsLightning = 3,
+        GotoFactory = 4,
+        GotoOTAMode = 5,
+    };
 
-typedef struct
-{
-    uint8_t u8Red;
-    uint8_t u8Green;
-    uint8_t u8Blue;
-} SGUBRPROTOCOL_SRGB;
+    struct sRGB
+    {
+        uint8_t u8Red;
+        uint8_t u8Green;
+        uint8_t u8Blue;
+    };
 
-typedef enum
-{
-    SGUBRPROTOCOL_ECHEVRONANIM_FadeIn = 0,
-    SGUBRPROTOCOL_ECHEVRONANIM_FadeOut = 1,
-    SGUBRPROTOCOL_ECHEVRONANIM_ErrorToWhite = 2,
-    SGUBRPROTOCOL_ECHEVRONANIM_ErrorToOff = 3,
-    SGUBRPROTOCOL_ECHEVRONANIM_AllSymbolsOn = 4,
-    SGUBRPROTOCOL_ECHEVRONANIM_Suicide = 5,
+    enum class EChevronAnimation
+    {
+        FadeIn = 0,
+        FadeOut = 1,
+        ErrorToWhite = 2,
+        ErrorToOff = 3,
+        AllSymbolsOn = 4,
+        Suicide = 5,
 
-    SGUBRPROTOCOL_ECHEVRONANIM_Count
-} SGUBRPROTOCOL_ECHEVRONANIM;
+        Count
+    };
 
-typedef struct
-{
-    SGUBRPROTOCOL_SRGB sRGB;
-    const uint8_t* u8Lights;
-    uint8_t u8LightCount;
-} SGUBRPROTOCOL_SUpdateLightArg;
+    struct SUpdateLightArg
+    {
+        sRGB sColor;
+        const uint8_t* u8Lights;
+        uint8_t u8LightCount;
+    };
 
-typedef struct
-{
-    uint32_t u32MaximumTimeMS;
-} SGUBRPROTOCOL_SKeepAliveArg;
+    struct SKeepAliveArg
+    {
+        uint32_t u32MaximumTimeMS;
+    };
 
-typedef struct
-{
-    SGUBRPROTOCOL_ECHEVRONANIM eChevronAnim;
-} SGUBRPROTOCOL_SChevronsLightningArg;
+    struct SChevronsLightningArg
+    {
+        EChevronAnimation eChevronAnim;
+    };
 
-typedef void(*fnUpdateLight)(const SGUBRPROTOCOL_SUpdateLightArg* psArg);
-typedef void(*fnTurnOff)(void);
-typedef void(*fnKeepAlive)(const SGUBRPROTOCOL_SKeepAliveArg* psKeepAliveArg);
-typedef void(*fnChevronsLightning)(const SGUBRPROTOCOL_SChevronsLightningArg* psChevronLightningArg);
+    typedef void(*fnUpdateLight)(const SUpdateLightArg* psArg);
+    typedef void(*fnTurnOff)(void);
+    typedef void(*fnKeepAlive)(const SKeepAliveArg* psKeepAliveArg);
+    typedef void(*fnChevronsLightning)(const SChevronsLightningArg* psChevronLightningArg);
 
-typedef void(*fnGotoFactory)();
+    typedef void(*fnGotoFactory)();
 
-typedef void(*fnGotoOTAMode)(void);
+    typedef void(*fnGotoOTAMode)(void);
 
-typedef struct
-{
-    fnKeepAlive fnKeepAliveHandler;
-    fnTurnOff fnTurnOffHandler;
+    typedef struct
+    {
+        fnKeepAlive fnKeepAliveHandler;
+        fnTurnOff fnTurnOffHandler;
 
-    fnUpdateLight fnUpdateLightHandler;
-    fnChevronsLightning fnChevronsLightningHandler;
+        fnUpdateLight fnUpdateLightHandler;
+        fnChevronsLightning fnChevronsLightningHandler;
 
-    fnGotoFactory fnGotoFactoryHandler;
+        fnGotoFactory fnGotoFactoryHandler;
 
-    fnGotoOTAMode fnGotoOTAModeHandler;
-} SGUBRPROTOCOL_SConfig;
+        fnGotoOTAMode fnGotoOTAModeHandler;
+    } SConfig;
 
-typedef struct
-{
-    const SGUBRPROTOCOL_SConfig* psConfig;
-} SGUBRPROTOCOL_SHandle;
+    class SGUComm
+    {
+        public:
+        static bool Decode(const SConfig& refConfig, const uint8_t* u8Datas, uint16_t u16Length);
 
-void SGUBRPROTOCOL_Init(SGUBRPROTOCOL_SHandle* psHandle, const SGUBRPROTOCOL_SConfig* psConfig);
-
-bool SGUBRPROTOCOL_Decode(const SGUBRPROTOCOL_SHandle* psHandle, const uint8_t* u8Datas, uint16_t u16Length);
-
-// Encode keep alive command.
-uint32_t SGUBRPROTOCOL_EncKeepAlive(uint8_t* u8Dst, uint16_t u16MaxLen, const SGUBRPROTOCOL_SKeepAliveArg* psArg);
-uint32_t SGUBRPROTOCOL_EncTurnOff(uint8_t* u8Dst, uint16_t u16MaxLen);
-uint32_t SGUBRPROTOCOL_EncUpdateLight(uint8_t* u8Dst, uint16_t u16MaxLen, const SGUBRPROTOCOL_SUpdateLightArg* psArg);
-uint32_t SGUBRPROTOCOL_EncChevronLightning(uint8_t* u8Dst, uint16_t u16MaxLen, const SGUBRPROTOCOL_SChevronsLightningArg* psArg);
-uint32_t SGUBRPROTOCOL_EncGotoFactory(uint8_t* u8Dst, uint16_t u16MaxLen);
-uint32_t SGUBRPROTOCOL_EncGotoOTAMode(uint8_t* u8Dst, uint16_t u16MaxLen);
-
-#ifdef __cplusplus
+        // Encode keep alive command.
+        static uint32_t EncKeepAlive(uint8_t* u8Dst, uint16_t u16MaxLen, const SKeepAliveArg* psArg);
+        static uint32_t EncTurnOff(uint8_t* u8Dst, uint16_t u16MaxLen);
+        static uint32_t EncUpdateLight(uint8_t* u8Dst, uint16_t u16MaxLen, const SUpdateLightArg* psArg);
+        static uint32_t EncChevronLightning(uint8_t* u8Dst, uint16_t u16MaxLen, const SChevronsLightningArg* psArg);
+        static uint32_t EncGotoFactory(uint8_t* u8Dst, uint16_t u16MaxLen);
+        static uint32_t EncGotoOTAMode(uint8_t* u8Dst, uint16_t u16MaxLen);
+    };
 }
-#endif
-
-#endif
