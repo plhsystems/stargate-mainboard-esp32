c610e99f2c1d9ac567d378109d369cc7a456d485
Max <pinkymaxou08@gmail.com>
2024-02-08 18:58:46 -0500

    OTA spacing

--- firmware/stargate-fw/main/WebServer/OTA_API.cpp
+++ firmware/stargate-fw/main/WebServer/OTA_API.cpp
@@ -7,82 +7,82 @@
 esp_err_t WebServer::OTAUploadPostHandler(httpd_req_t *req)
 {
     {
-        ESP_LOGI(TAG, "OTAUploadPostHandler / uri: %s", req->uri);
+    ESP_LOGI(TAG, "OTAUploadPostHandler / uri: %s", req->uri);
 
-        const esp_partition_t *configured = esp_ota_get_boot_partition();
-        const esp_partition_t *running = esp_ota_get_running_partition();
+    const esp_partition_t *configured = esp_ota_get_boot_partition();
+    const esp_partition_t *running = esp_ota_get_running_partition();
 
-        if (configured != running)
-        {
-            ESP_LOGW(TAG, "Configured OTA boot partition at offset 0x%08x, but running from offset 0x%08x",
-                (int)configured->address, (int)running->address);
-            ESP_LOGW(TAG, "(This can happen if either the OTA boot data or preferred boot image become corrupted somehow.)");
-        }
+    if (configured != running)
+    {
+        ESP_LOGW(TAG, "Configured OTA boot partition at offset 0x%08x, but running from offset 0x%08x",
+            (int)configured->address, (int)running->address);
+        ESP_LOGW(TAG, "(This can happen if either the OTA boot data or preferred boot image become corrupted somehow.)");
+    }
 
-        ESP_LOGI(TAG, "Running partition type %d subtype %d (offset 0x%08x)",
-                (int)running->type, (int)running->subtype, (int)running->address);
+    ESP_LOGI(TAG, "Running partition type %d subtype %d (offset 0x%08x)",
+            (int)running->type, (int)running->subtype, (int)running->address);
 
-        const esp_partition_t* update_partition = esp_ota_get_next_update_partition(NULL);
-        assert(update_partition != NULL);
-        ESP_LOGI(TAG, "Writing to partition subtype %d at offset 0x%x",
-                (int)update_partition->subtype, (int)update_partition->address);
+    const esp_partition_t* update_partition = esp_ota_get_next_update_partition(NULL);
+    assert(update_partition != NULL);
+    ESP_LOGI(TAG, "Writing to partition subtype %d at offset 0x%x",
+            (int)update_partition->subtype, (int)update_partition->address);
 
-        esp_ota_handle_t update_handle = 0;
+    esp_ota_handle_t update_handle = 0;
 
-        esp_err_t err = esp_ota_begin(update_partition, OTA_WITH_SEQUENTIAL_WRITES, &update_handle);
-        err = esp_ota_begin(update_partition, OTA_WITH_SEQUENTIAL_WRITES, &update_handle);
+    esp_err_t err = esp_ota_begin(update_partition, OTA_WITH_SEQUENTIAL_WRITES, &update_handle);
+    err = esp_ota_begin(update_partition, OTA_WITH_SEQUENTIAL_WRITES, &update_handle);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "esp_ota_begin failed (%s)", esp_err_to_name(err));
+        esp_ota_abort(update_handle);
+        goto ERROR;
+    }
+
+    int n = httpd_req_recv(req, (char*)getI().m_u8Buffers, HTTPSERVER_BUFFERSIZE);
+    int binary_file_length = 0;
+
+    while(n > 0)
+    {
+        ESP_LOGI(TAG, "OTAUploadPostHandler / receiving: %d bytes", (int)n);
+
+        err = esp_ota_write( update_handle, (const void *)getI().m_u8Buffers, n);
         if (err != ESP_OK)
         {
-            ESP_LOGE(TAG, "esp_ota_begin failed (%s)", esp_err_to_name(err));
             esp_ota_abort(update_handle);
             goto ERROR;
         }
+        binary_file_length += n;
+        ESP_LOGD(TAG, "Written image length %d", (int)binary_file_length);
 
-        int n = httpd_req_recv(req, (char*)getI().m_u8Buffers, HTTPSERVER_BUFFERSIZE);
-        int binary_file_length = 0;
+        n = httpd_req_recv(req, (char*)getI().m_u8Buffers, HTTPSERVER_BUFFERSIZE);
+    }
 
-        while(n > 0)
-        {
-            ESP_LOGI(TAG, "OTAUploadPostHandler / receiving: %d bytes", (int)n);
-
-            err = esp_ota_write( update_handle, (const void *)getI().m_u8Buffers, n);
-            if (err != ESP_OK)
-            {
-                esp_ota_abort(update_handle);
-                goto ERROR;
-            }
-            binary_file_length += n;
-            ESP_LOGD(TAG, "Written image length %d", (int)binary_file_length);
-
-            n = httpd_req_recv(req, (char*)getI().m_u8Buffers, HTTPSERVER_BUFFERSIZE);
+    err = esp_ota_end(update_handle);
+    if (err != ESP_OK)
+    {
+        if (err == ESP_ERR_OTA_VALIDATE_FAILED) {
+            ESP_LOGE(TAG, "Image validation failed, image is corrupted");
+        } else {
+            ESP_LOGE(TAG, "esp_ota_end failed (%s)!", esp_err_to_name(err));
         }
 
-        err = esp_ota_end(update_handle);
-        if (err != ESP_OK)
-        {
-            if (err == ESP_ERR_OTA_VALIDATE_FAILED) {
-                ESP_LOGE(TAG, "Image validation failed, image is corrupted");
-            } else {
-                ESP_LOGE(TAG, "esp_ota_end failed (%s)!", esp_err_to_name(err));
-            }
+        // TODO: esp_ota_abort(update_handle); needed ?
+        goto ERROR;
+    }
 
-            // TODO: esp_ota_abort(update_handle); needed ?
-            goto ERROR;
-        }
+    err = esp_ota_set_boot_partition(update_partition);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "esp_ota_set_boot_partition failed (%s)!", esp_err_to_name(err));
+        goto ERROR;
+    }
 
-        err = esp_ota_set_boot_partition(update_partition);
-        if (err != ESP_OK)
-        {
-            ESP_LOGE(TAG, "esp_ota_set_boot_partition failed (%s)!", esp_err_to_name(err));
-            goto ERROR;
-        }
+    ESP_LOGI(TAG, "OTA Completed !");
+    ESP_LOGI(TAG, "Prepare to restart system!");
 
-        ESP_LOGI(TAG, "OTA Completed !");
-        ESP_LOGI(TAG, "Prepare to restart system!");
-
-        // TODO: Restart
-        httpd_resp_set_hdr(req, "Connection", "close");
-        return ESP_OK;
+    // TODO: Restart
+    httpd_resp_set_hdr(req, "Connection", "close");
+    return ESP_OK;
     }
     ERROR:
     httpd_resp_set_hdr(req, "Connection", "close");
