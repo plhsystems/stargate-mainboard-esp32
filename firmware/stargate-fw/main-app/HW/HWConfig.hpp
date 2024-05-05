2ff909768c7663b042ea599dec5fa0874648651f
Max <pinkymaxou08@gmail.com>
2024-05-05 18:24:45 -0400

    Remove no longer used file

--- firmware/stargate-fw/main-app/HW/HWConfig.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "driver/gpio.h"
-
-// Board select
-#define HWCONFIG_BOARD_PINKYBOARD (1)
-#define HWCONFIG_BOARD_PABLOBOARD (2)
-
-#define HWCONFIG_BOARD_CURRENT (HWCONFIG_BOARD_PINKYBOARD)
