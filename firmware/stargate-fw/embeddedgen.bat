92dbd84fc83d83a924818416d30eb820e5d60cb9
Max <pinkymaxou08@gmail.com>
2024-05-07 20:39:47 -0400

    Move some functions

---
+++ firmware/stargate-fw/embeddedgen.bat
@@ -0,0 +1,2 @@
+python ..\tools\embeddedgen.py -i ".\main-app\webserver-assets" -o ".\main-app\WebServer"
+REM python ..\tools\embeddedgen.py -i ".\soundfx-assets" -o ".\main\Audio"
--- firmware/stargate-fw/pinky-board/embeddedgen.bat
+++
@@ -1,2 +0,0 @@
-python ..\tools\embeddedgen.py -i ".\webserver-assets" -o ".\main\webserver"
-REM python ..\tools\embeddedgen.py -i ".\soundfx-assets" -o ".\main\Audio"
